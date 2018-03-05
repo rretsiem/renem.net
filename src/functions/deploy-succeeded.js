57929de9fa6addec0d176278264edc184d89f7b4
René Meister <rene@renem.net>
2018-03-05 17:56:52 +0100

    Instagram webhook - first try via Lambda function

--- src/functions/deploy-succeeded.js
+++ src/functions/deploy-succeeded.js
@@ -1,4 +1,4 @@
-const request = require('request');
+const request = require('request').post;
 
 exports.handler = function(event, context, callback) {
   var e = JSON.parse(event.body);
---
+++ static/images/instagram/readme.txt
@@ -0,0 +1,1 @@
+// placeholder for images imported from instagram
