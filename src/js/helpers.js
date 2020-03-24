e7ef3713be83573ceacb4e93ded3612253dc6b90
René Meister <rene@renem.net>
2020-03-24 10:49:22 +0100

    added pixelfed as rel=me

--- src/js/helpers.js
+++
@@ -1,9 +0,0 @@
-// Add event listeners once the DOM has fully loaded by listening for the
-// `DOMContentLoaded` event on the document, and adding your listeners to
-// specific elements when it triggers.
-/*
-document.addEventListener('DOMContentLoaded', function () {
-  document.getElementById('subToMeLink').addEventListener('click', subToMe);
-  // main();
-});
-*/
